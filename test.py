5678c48737faf8b336ee93d4d87a9e4b57c43b67
JuwanHan <juwoan0601@gmail.com>
2022-01-13 00:11:05 +0900

    Mastering the functions

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 """
 Example code of using forcast
 """
-from re import T
 import pandas as pd
 import numpy as np
 from config import TRAIN_DATASET_PATH
 
 df_train = pd.read_csv(TRAIN_DATASET_PATH, encoding='utf-8')
 
-from forecast import ML
-ML.random_forest(df_train)
+# from forecast import ML
+# ML.tpot_34_column(df_train,save=True)
+
+import pickle
+MODEL_PATH = './saved_model/tpot_34_column_26.19.pkl'
+loaded_model = pickle.load(open(MODEL_PATH, 'rb'))
+a = [2022,1,3,17,30,4,  0,0,0,0,0,0,0,0,0,0,0,0,    25,0,0,0,0,0,0,0,0,0,0,0,   0,0,0,0]
+input = np.reshape(np.array(a),(1,-1))
+result = loaded_model.predict(input)
+print("예상 식수인원은 ", int(result), "명 입니다.")
